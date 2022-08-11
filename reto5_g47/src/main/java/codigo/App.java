616d5354e24f789ce9ce4369d20fa902967f0336
Jhonba <105740491+jhonba740426@users.noreply.github.com>
2022-08-11 09:08:11 -0500

    Reto5_Jhon_Ayala_Grupo47

---
+++ reto5_g47/src/main/java/codigo/App.java
@@ -0,0 +1,18 @@
+package codigo;
+import codigo.Modelo.VO.Consulta1VO;
+//import codigo.Modelo.VO.*;
+import codigo.Vista.*;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        //System.out.println( "Hello World!" );
+        Consulta1VO.valores();
+        new Interfazp();
+
+    }
+}
